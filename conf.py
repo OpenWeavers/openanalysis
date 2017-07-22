d98830b4244f84305387fc094b480a450529b023
GiZmoFalcon <darojisourabh@ymail.com>
2017-07-22 11:31:14 +0530

    Added favicon

--- conf.py
+++ conf.py
@@ -94,6 +94,7 @@ todo_include_todos = False
 #
 # html_theme_options = {}
 html_logo = 'res/OpenAnalysis1080.png'
+html_favicon = 'res/icon.ico'
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
