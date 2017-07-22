d918eab9fdf1551c96d904cd9502afde4f458201
Vinayaka K V <me.vinayakakv@gmail.com>
2017-07-22 11:47:00 +0530

    Update conf.py

--- conf.py
+++ conf.py
@@ -112,7 +112,7 @@ htmlhelp_basename = 'OpenAnalysisdoc'
 latex_elements = {
     # The paper size ('letterpaper' or 'a4paper').
     #
-    papersize': 'a4paper'
+    'papersize': 'a4paper'
 
     # The font size ('10pt', '11pt' or '12pt').
     #
