95470e6603e9e8c0a14c7a1fe79e96e81913c35d
vinayaka <me.vinayakakv@gmail.com>
2017-07-22 11:21:58 +0530

    Reformatted file

--- setup.py
+++ setup.py
@@ -5,28 +5,28 @@ import sys
 from platform import platform
 
 if sys.version_info < (3, 5):
-	sys.exit('Sorry, Python < 3.5 is not supported')
+    sys.exit('Sorry, Python < 3.5 is not supported')
 if 'Ubuntu' in platform():
-	os.system("sudo apt-get install python3-tk")
+    os.system("sudo apt-get install python3-tk")
 elif 'Fedora' in platform():
-	os.system("sudo yum install python3-tk")
+    os.system("sudo yum install python3-tk")
 else:
-	sys.stderr.write(
-	"""
-	Error...
-	Unsupported OS...
-	Download and install python3-tk for your specific OS
-	""")
-	sys.exit(0)
+    sys.stderr.write(
+        """
+        Error...
+        Unsupported OS...
+        Download and install python3-tk for your specific OS
+        """)
+    sys.exit(0)
 
 setup(
-    name = "OpenAnalysis",
-    version = "0.0.1",
-    author = "OpenWeavers",
-    description = ("An open source package to analyse and visualise algorithms and data structures"),
-    license = "GNU",
-    keywords = "OpenWeavers product",
-    url = "http://openalgorithm.readthedocs.io",
+    name="OpenAnalysis",
+    version="0.0.1",
+    author="OpenWeavers",
+    description="An open source package to analyse and visualise algorithms and data structures",
+    license="GNU",
+    keywords="OpenWeavers product",
+    url="http://openalgorithm.readthedocs.io",
     packages=['OpenAnalysis'],
     classifiers=[
         "Development Status :: 3 - Alpha",
@@ -34,14 +34,14 @@ setup(
         "License :: OSI Approved :: GNU License",
     ],
     install_requires=[
-	'networkx',
-	'numpy',
-	'matplotlib',
+        'networkx',
+        'numpy',
+        'matplotlib',
     ],
     extras_require={
-	"extensions" : [
-            'jupyter', 
+        "extensions": [
+            'jupyter',
             'ipython',
         ],
-    },	
+    },
 )
