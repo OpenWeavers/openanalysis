8314cb24a515fa454f4f067e941ce4a2a77f4fc5
Vinayaka K V <me.vinayakakv@gmail.com>
2017-07-22 11:00:43 +0530

    Update conf.py

--- conf.py
+++ conf.py
@@ -93,7 +93,7 @@ todo_include_todos = False
 # documentation.
 #
 # html_theme_options = {}
-html_logo = ./res/OpenAnalysis1080.png
+html_logo = res/OpenAnalysis1080.png
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
