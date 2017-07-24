608f358c4d66616caa0f6342a9bcdf04305ff336
Vinayaka K V <me.vinayakakv@gmail.com>
2017-07-24 18:50:45 +0530

    testing svg formats

--- conf.py
+++ conf.py
@@ -33,7 +33,8 @@
 extensions = ['nbsphinx',
               'sphinx.ext.autodoc',
               'sphinx.ext.imgmath',
-	      'sphinx.ext.githubpages'
+	      'sphinx.ext.githubpages',
+	      'sphinx.ext.imgconverter'
              ]
 
 # Add any paths that contain templates here, relative to this directory.
