0d1eee39837d12dd8f12d39f57eb602dedc81c61
Syed TS <syedtysh@gmail.com>
2017-07-27 20:41:01 +0530

    updated __init__.py
    code fix for printing the package dependency only once

--- openanalysis/__init__.py
+++ openanalysis/__init__.py
@@ -1,10 +1,16 @@
-pkgs = ["libgraphviz-dev", "pkg-config", "python3-tk", "ffmpeg"]
+import os
 
-print(
-    """
-    The OpenAlgorithm library needs {0}, {1}, {2}, and {3}
-    packages to be installed for graph visualization.
+pkgs = []
+
+if not os.path.exists("check_file"):
+	
+	print(
+	"""
+    The OpenAlgorithm library needs 'libgraphviz-dev', 'pkg-config', 'python3-tk', 
+    and 'ffmpeg' packages to be installed for graph visualization.
    
     Install them before continuing...
-    """
-.format(pkgs))
+    """)
+	
+	with open("check_file", 'w') as f:
+		f.write("")
