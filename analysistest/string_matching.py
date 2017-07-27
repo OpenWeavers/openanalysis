62687aaf3e61b11a115fa118278b289a4180afd5
Vinyas N S <vinyas.n.s@gmail.com>
2017-07-27 19:51:53 +0530

    reformat file string_matching.py

--- analysistest/string_matching.py
+++ analysistest/string_matching.py
@@ -5,6 +5,7 @@ class BruteForceMatch(StringMatchingAlgorithm):
     """
     Class to implement Brute Force String Matching Algorithm
     """
+
     def __init__(self):
         StringMatchingAlgorithm.__init__(self, "Brute Force String Matching")
 
@@ -46,80 +47,80 @@ class Horspool(StringMatchingAlgorithm):
         i = len(self.pattern) - 1
         while i < len(text):
             j = 0
-            while j < len(self.pattern) and text[i-j] == self.pattern[len(self.pattern)-1-j]:
+            while j < len(self.pattern) and text[i - j] == self.pattern[len(self.pattern) - 1 - j]:
                 j += 1
             self.count += j
             if j == len(self.pattern):
-                return i-len(self.pattern)+1
+                return i - len(self.pattern) + 1
             if text[i] in self.shift_table:
                 i += self.shift_table[text[i]]
             else:
                 i += len(self.pattern)
         return -1
-    
+
 
 class BoyerMoore(StringMatchingAlgorithm):
-	def __init__(self):
-		StringMatchingAlgorithm.__init__(self, "Boyer-Moore String Matching")
-		self.gdsuf_shift_table = []
-		self.bdsym_shift_table = {}
-		self.pattern = ''
+    def __init__(self):
+        StringMatchingAlgorithm.__init__(self, "Boyer-Moore String Matching")
+        self.gdsuf_shift_table = []
+        self.bdsym_shift_table = {}
+        self.pattern = ''
 
-	def generate_bdsym_shift_table(self, pattern):
-		self.pattern = pattern
-		for i in range(len(pattern) - 1):
-			self.bdsym_shift_table[pattern[i]] = len(pattern) - i - 1
-		
-	def generate_gdsuf_shift_table(self, pattern):
-		lp = len(pattern)
-		for i in range(lp-1):
-			suff = pattern[lp-i-1:]
-			# find highest index where suffix 'suff' is present in pattern
-			targ = pattern.rfind(suff, 0, lp-i-1)
-			while pattern[targ-1] == pattern[lp-i-2] and targ > 0:
-				targ = pattern.rfind(suff, 0, targ)
-			if targ > 0:
-				self.gdsuf_shift_table.append((lp-i-1) - targ)
-			elif targ == 0:
-				self.gdsuf_shift_table.append(lp-i-1)
-			else:
-				lis = []
-				# find first and last pre and suff that should match, then append distance b/w them
-				for j in range(lp-1, lp-i-2,-1):
-					if pattern[j:] == pattern[0:lp-j]:
-						lis.append(j)
-				if len(lis) == 0:
-					self.gdsuf_shift_table.append(lp)
-				else:
-					self.gdsuf_shift_table.append(max(lis))
+    def generate_bdsym_shift_table(self, pattern):
+        self.pattern = pattern
+        for i in range(len(pattern) - 1):
+            self.bdsym_shift_table[pattern[i]] = len(pattern) - i - 1
 
-	def match(self, text: str, pattern: str):
-		StringMatchingAlgorithm.match(self, text, pattern)
-		# pattern = "BAOBAB"
-		self.generate_bdsym_shift_table(pattern)
-		self.generate_gdsuf_shift_table(pattern)
-		# print(self.gdsuf_shift_table, self.bdsym_shift_table)
-		i = len(self.pattern) - 1
-		while i < len(text):
-			j = 0
-			k = 0	# matched characters
-			while j < len(self.pattern) and text[i-j] == self.pattern[len(self.pattern)-j-1]:
-				j += 1
-				k += 1
-			self.count += j
-			if j == len(self.pattern):
-				return i-len(self.pattern)+1
-			if text[i-j] in self.bdsym_shift_table:
-				t1 = self.bdsym_shift_table[text[i-j]]
-			else:
-				t1 = len(self.pattern)
-			d1 = max(t1 - k, 1)
-			d = d1
-			if k > 0:
-				d2 = self.gdsuf_shift_table[k-1]	# 0 based index table
-				d = max(d1, d2)
-			i += d
-		return -1
+    def generate_gdsuf_shift_table(self, pattern):
+        lp = len(pattern)
+        for i in range(lp - 1):
+            suff = pattern[lp - i - 1:]
+            # find highest index where suffix 'suff' is present in pattern
+            targ = pattern.rfind(suff, 0, lp - i - 1)
+            while pattern[targ - 1] == pattern[lp - i - 2] and targ > 0:
+                targ = pattern.rfind(suff, 0, targ)
+            if targ > 0:
+                self.gdsuf_shift_table.append((lp - i - 1) - targ)
+            elif targ == 0:
+                self.gdsuf_shift_table.append(lp - i - 1)
+            else:
+                lis = []
+                # find first and last pre and suff that should match, then append distance b/w them
+                for j in range(lp - 1, lp - i - 2, -1):
+                    if pattern[j:] == pattern[0:lp - j]:
+                        lis.append(j)
+                if len(lis) == 0:
+                    self.gdsuf_shift_table.append(lp)
+                else:
+                    self.gdsuf_shift_table.append(max(lis))
+
+    def match(self, text: str, pattern: str):
+        StringMatchingAlgorithm.match(self, text, pattern)
+        # pattern = "BAOBAB"
+        self.generate_bdsym_shift_table(pattern)
+        self.generate_gdsuf_shift_table(pattern)
+        # print(self.gdsuf_shift_table, self.bdsym_shift_table)
+        i = len(self.pattern) - 1
+        while i < len(text):
+            j = 0
+            k = 0  # matched characters
+            while j < len(self.pattern) and text[i - j] == self.pattern[len(self.pattern) - j - 1]:
+                j += 1
+                k += 1
+            self.count += j
+            if j == len(self.pattern):
+                return i - len(self.pattern) + 1
+            if text[i - j] in self.bdsym_shift_table:
+                t1 = self.bdsym_shift_table[text[i - j]]
+            else:
+                t1 = len(self.pattern)
+            d1 = max(t1 - k, 1)
+            d = d1
+            if k > 0:
+                d2 = self.gdsuf_shift_table[k - 1]  # 0 based index table
+                d = max(d1, d2)
+            i += d
+        return -1
 
 
 if __name__ == "__main__":
