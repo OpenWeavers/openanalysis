9a0181129b4c67dd73c8b343e7044592f5c8e421
Vinyas N S <vinyas.n.s@gmail.com>
2017-07-28 09:44:04 +0530

    fixed typos and reformat file __init__.py

--- openanalysis/__init__.py
+++ openanalysis/__init__.py
@@ -3,14 +3,13 @@ import os
 pkgs = []
 
 if not os.path.exists("check_file"):
-	
-	print(
-	"""
+    print(
+        """
     The OpenAlgorithm library needs 'libgraphviz-dev', 'pkg-config', 'python3-tk', 
     and 'ffmpeg' packages to be installed for graph visualization.
    
     Install them before continuing...
     """)
-	
-	with open("check_file", 'w') as f:
-		f.write("")
+
+    with open("check_file", 'w') as f:
+        f.write("")
