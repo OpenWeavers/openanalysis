ea1e6134b04e9a0d3e02f3daf80e7eb4aac3eb65
Monish S R <srmonish96@gmail.com>
2017-08-21 17:02:08 +0000

    Updated datastructures.py
    Added working window

--- openanalysis/datastructures.py
+++ openanalysis/datastructures.py
@@ -1,17 +1,12 @@
-import warnings
-
-import networkx as nx
-import matplotlib.pyplot as plt
+import pygraphviz as pgv
 import gi
-
+import os
 gi.require_version("Gtk", "3.0")
 from gi.repository import Gtk as gtk
-import os
-
 '''
 Usage Instructions:
 
->sudo apt install python-gi-cairo
+>sudo apt install python3-dev
 >put sd.glade in same working directory
 >in main(),pass any instance of DataStructureBase
 
@@ -30,11 +25,9 @@ class DataStructureBase:
         :param file_path: Path to store output of DS
         If the name contains 'tree', then layout is tree layout, else Graph
         """
-        warnings.warn('This module is deprecated ... Wait till v2.0')
         self.name = name
         self.is_tree = "TREE" in name.upper() or "HEAP" in name.upper()
-        self.layout = self.__binary_tree_layout if self.is_tree else self.__hierarchy_pos
-        self.graph = nx.Graph()
+        self.graph = {}
         self.file_path = file_path
         print(os.path.abspath(file_path))
         # Layout to draw BFS tree
@@ -74,100 +67,32 @@ class DataStructureBase:
         """
         pass
 
-    def draw(self):
-        """
-        ----old-----
-        Do a BFS and draw the Data Structure
-        Data Structure is essentially graph like and can be represented by Mathematical Relation
-        For example Graph G : 1--2--3 can be represented as R_G = {(1,2),(2,3)}
-        In python they can be represented as Set, whose elements are tuples
-        At last, set can be transformed into list, and a graph can be created
-        example:
-            >>> s = {(1,2),(2,3),(1,3)}
-            >>> r = list(s)
-            >>> G = nx.Graph(r)
+    def get_graph(self, rt):
+        pass
 
-        Such a set can be crated during operations or a BFS on Data Structure by updating self
-        -----new-----
-        plots self.graph, saves the image and returns the path to saved image
-        """
-        Tree = self.graph
-        if Tree.nodes():
-            plt.clf()
-            pos = self.layout(Tree, self.get_root())
-            nx.draw(Tree, pos, with_labels=True)
-            plt.savefig(self.file_path)
-        return self.file_path
-
-    @staticmethod
-    def __binary_tree_layout(G, root, width=1., vert_gap=0.2, vert_loc=0., xcenter=0.5,
-                             pos=None, parent=None):
-        """If there is a cycle that is reachable from root, then this will see infinite recursion.
-           G: the graph
-           root: the root node of current branch
-           width: horizontal space allocated for this branch - avoids overlap with other branches
-           vert_gap: gap between levels of hierarchy
-           vert_loc: vertical location of root
-           xcenter: horizontal location of root
-           pos: a dict saying where all nodes go if they have been assigned
-           parent: parent of this branch.
-           each node has an attribute "left: or "right\""""
-        if pos is None:
-            pos = {root: (xcenter, vert_loc)}
+    def draw(self, nth=None):
+        if self.get_root() is None:
+            print "No entries yet!"
         else:
-            pos[root] = (xcenter, vert_loc)
-        neighbors = G.neighbors(root)
-        if parent is not None:
-            neighbors.remove(parent)
-        if len(neighbors) != 0:
-            dx = width / 2.
-            leftx = xcenter - dx / 2
-            rightx = xcenter + dx / 2
-            for neighbor in neighbors:
-                if G.node[neighbor]['child_status'] == 'left':
-                    pos = DataStructureBase.__binary_tree_layout(G, neighbor, width=dx, vert_gap=vert_gap,
-                                                                 vert_loc=vert_loc - vert_gap, xcenter=leftx, pos=pos,
-                                                                 parent=root)
-                elif G.node[neighbor]['child_status'] == 'right':
-                    pos = DataStructureBase.__binary_tree_layout(G, neighbor, width=dx, vert_gap=vert_gap,
-                                                                 vert_loc=vert_loc - vert_gap, xcenter=rightx, pos=pos,
-                                                                 parent=root)
-        return pos
-
-    @staticmethod
-    def __hierarchy_pos(G, root, width=1., vert_gap=0.2, vert_loc=0, xcenter=0.5):
-        '''If there is a cycle that is reachable from root, then result will not be a hierarchy.
-
-           G: the graph
-           root: the root node of current branch
-           width: horizontal space allocated for this branch - avoids overlap with other branches
-           vert_gap: gap between levels of hierarchy
-           vert_loc: vertical location of root
-           xcenter: horizontal location of root
-        '''
-
-        def h_recur(G, root, width=1., vert_gap=0.2, vert_loc=0., xcenter=0.5,
-                    pos=None, parent=None, parsed=[]):
-            if (root not in parsed):
-                parsed.append(root)
-                if pos == None:
-                    pos = {root: (xcenter, vert_loc)}
+            code = lambda x: "n" + str(abs(x)) if x < 0 else "p" + str(abs(x))
+            self.get_graph(self.get_root())
+            dfile = open("test_dot.dot", "w")
+            dfile.write("strict digraph {\n\tnode [shape = record,height=.1];\n")
+            for key in self.graph:
+                if nth is not None and nth==key:
+                    dfile.write("\tnode{0} [label = \"<f0> |<f1> {1}|<f2> \"] [color = \"red\"];\n".format(code(key), key))
                 else:
-                    pos[root] = (xcenter, vert_loc)
-                neighbors = G.neighbors(root)
-                if parent != None:
-                    neighbors.remove(parent)
-                if len(neighbors) != 0:
-                    dx = width / len(neighbors)
-                    nextx = xcenter - width / 2 - dx / 2
-                    for neighbor in neighbors:
-                        nextx += dx
-                        pos = h_recur(G, neighbor, width=dx, vert_gap=vert_gap,
-                                      vert_loc=vert_loc - vert_gap, xcenter=nextx, pos=pos,
-                                      parent=root, parsed=parsed)
-            return pos
+                    dfile.write("\tnode{0} [label = \"<f0> |<f1> {1}|<f2> \"];\n".format(code(key), key))
 
-        return h_recur(G, root, width=1., vert_gap=0.2, vert_loc=0, xcenter=0.5)
+            for key in self.graph:
+                for value in self.graph[key]:
+                    dfile.write("\t\"node{0}\":{1}->\"node{2}\":f1;\n".format(code(key), "f0"
+                    if self.graph[key][value]['child_status'] == 'left'
+                    else "f2", code(value)))
+
+            dfile.write("}")
+            dfile.close()
+            pgv.AGraph("test_dot.dot").draw(self.file_path, prog="dot", format="png")
 
 
 class DataStructureVisualization:
@@ -201,7 +126,17 @@ class DataStructureVisualization:
             ele = int(self.builder.get_object("item").get_text())
             choice = int(self.builder.get_object("operation").get_active())
             state = self.builder.get_object("state")
-            self.map[choice](ele)
-            state.set_from_file(self.ds.draw())
-        except Exception as e:
+            val = self.map[choice](ele)
+            if val is False:  # Search or Delete Case
+                dialog = gtk.MessageDialog(None, 0, gtk.MessageType.ERROR,
+                                           gtk.ButtonsType.CANCEL, "Value not found ERROR")
+                dialog.format_secondary_text(
+                    "Element not found in the %s"%self.ds.name)
+                dialog.run()
+                dialog.destroy()
+            else:
+                val = ele if val is True else None
+                self.ds.draw(val)
+                state.set_from_file(self.ds.file_path)
+        except:
             raise
