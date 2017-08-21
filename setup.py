306587bd80ddd1a30cdd56c10b1ce63895280e40
Monish S R <srmonish96@gmail.com>
2017-08-21 03:33:44 +0000

    Updated setup.py
    Added kivy dependency

--- setup.py
+++ setup.py
@@ -53,11 +53,12 @@ setup(
         "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
     ],
     install_requires=[
-        'networkx',
         'numpy',
         'matplotlib',
         'pygraphviz',
         'progressbar2',
+        'kivy',
+        'networkx',
     ],
     extras_require={
         "extensions": [
